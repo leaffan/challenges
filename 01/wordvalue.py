8b3352e35e7c5bd885001337dd00c890e97bea9b
leaffan <leaffan@gmx.net>
2017-12-07 08:51:31 +0100

    Improve previous solution, add comments

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -7,6 +7,7 @@ def load_words():
     """
     Loads dictionary into a list and returns list.
     """
+    # using a with statement to make sure the file is properly closed
     with open(DICTIONARY) as file:
         return [l.strip() for l in file.readlines()]
 
@@ -16,11 +17,14 @@ def calc_word_value(word, return_word=False):
     Calculates the value of the word entered into function using imported
     constant mapping LETTER_SCORES. Optionally returns the word itself, too.
     """
-    word_value = sum([LETTER_SCORES[
-        letter.upper()] for letter in word if letter.upper() in LETTER_SCORES])
+    # using get function on the dictionary to yield a default value of 0 if
+    # current character is not among the dictionary's keys
+    word_value = sum(LETTER_SCORES.get(char, 0) for char in word.upper())
 
+    # returning only the word value by default
     if not return_word:
         return word_value
+    # otherwise returning word and word value
     else:
         return word, word_value
 
@@ -34,7 +38,7 @@ def max_word_value(words=None, threaded=True):
     if words is None:
         words = load_words()
 
-    # calculating words simultaneously by default
+    # calculating word values simultaneously by default
     if threaded:
         word_values = list()
         with ThreadPoolExecutor(max_workers=4) as threads:
@@ -49,7 +53,7 @@ def max_word_value(words=None, threaded=True):
                     print("Conccurrent task generated an exception: %s" % e)
 
         return sorted(word_values, key=itemgetter(1), reverse=True).pop(0)[0]
-    # otherwise calculating words sequentially
+    # otherwise calculating word values sequentially
     else:
         for word in words:
             values = [calc_word_value(word) for word in words]
@@ -57,4 +61,4 @@ def max_word_value(words=None, threaded=True):
 
 
 if __name__ == "__main__":
-    print(max_word_value(('bob', 'julian', 'pybites', 'quit', 'barbeque')))
+    pass
